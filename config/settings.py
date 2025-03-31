bae218dcda40f82d7db177f25e4d00d3df2a0360
vijayagopalsb <vijayagopal.sb@gmail.com>
2025-03-31 11:40:32 +0530

    Initial Project setup with Hexagonal Architecture

---
+++ config/settings.py
@@ -0,0 +1,18 @@
+# File: config/settings.py
+
+# Configuration setting for project
+
+# Input image size
+IMAGE_SUZE = (128, 128)
+
+BATCH_SIZE = 32
+
+EPOCHS = 20
+
+# Noise level for training
+NOISE_FACTOR = 0.2
+
+DATASET_PATH =  ""
+
+MODEL_SAVE_PATH = "models/denoising_autoencoder.h5"
+
